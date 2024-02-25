59881cb70bdb99ddaa0bfca4ef1c06b78db818f0
Nhu-Nguyen <zennguyen2511@gmail.com>
2024-02-24 23:15:36 -0600

    adding files

---
+++ weatherapp/src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    'X-RapidAPI-Key': '37064df805mshcaaa5eb31993ee6p17ffb0jsna6af2d5e0d60',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+  }
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "ea081e7c8189b40b973d3d4c71f263d0"; 
